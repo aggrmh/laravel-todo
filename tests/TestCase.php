e594568d0393b6e8c350c4dceacf10ee537d62b4
aggrmh <mhkawsdd1027@gmail.com>
2021-11-19 18:05:30 +0900

    テストケース作成途中

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,10 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\Feature\Tasktest;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
 }
